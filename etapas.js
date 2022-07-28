bd1d9d50378a0f8846144a24bd7ccb57c1e7b3a8
Ruben andré <rubandre14@gmail.com>
2022-07-28 16:56:54 +0100

    first commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: "Fulano de tal",
+                partido: "ABC",
+                fotos: [
+                    { url: "foto1.jpg", legenda: "vereador" }
+                ]
+            },
+            {
+                numero: 77222,
+                nome: "Beltrano da Silva",
+                partido: "DEFG",
+                fotos: [
+                    { url: "foto2.jpg", legenda: "vereador" }
+                ]
+            },
+
+        ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: "Ciclano",
+                partido: "ABC",
+                vice: "cic",
+                fotos: [
+                    { url: "foto3.jpg", legenda: "Prefeito" },
+                    { url: "foto4.jpg", legenda: "Vice-Prefeito", small: true }
+                ]
+            },
+            {
+                numero: 84,
+                nome: "Zulano",
+                partido: "QWERTY",
+                vice: "Zul",
+                fotos: [
+                    { url: "foto5.jpg", legenda: "Prefeito" },
+                    { url: "foto6.jpg", legenda: "Vice-Prefeito", small: true }
+                ]
+            },
+        ]
+    }
+]
